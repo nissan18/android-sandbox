4a3d0ca8b3d1ec826b2172a55970de5c1c6e46df
Michael <nissan18@gmail.com>
2017-04-02 22:33:01 -0400

    Use ListView for main menu

--- AndroidSandbox/app/src/main/java/com/mishasoft/androidsandbox/BluetoothActivity.java
+++ AndroidSandbox/app/src/main/java/com/mishasoft/androidsandbox/BluetoothActivity.java
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,8 +26,8 @@ import android.widget.TextView;
         textViewDump.append("onCreate\n");
 //        Log.d(getLocalClassName(), "onCreate");
 
-//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-//        registerReceiver(mReceiver, filter);
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
     }
 
     public void checkStatus(View view) {
@@ -74,10 +75,7 @@ import android.widget.TextView;
         }
     }
 
-
-
-
-        // Create a BroadcastReceiver for ACTION_FOUND.
+    // Create a BroadcastReceiver for ACTION_FOUND.
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             TextView textViewDump = (TextView) findViewById(R.id.textViewDump);
@@ -88,9 +86,15 @@ import android.widget.TextView;
                 // Discovery has found a device. Get the BluetoothDevice
                 // object and its info from the Intent.
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
+                textViewDump.append(String.format("name = %s, MAC address = %s\n", device.getName(), device.getAddress()));
             }
         }
     };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(mReceiver);
+    }
 }
--- AndroidSandbox/app/src/main/java/com/mishasoft/androidsandbox/MainActivity.java
+++ AndroidSandbox/app/src/main/java/com/mishasoft/androidsandbox/MainActivity.java
@@ -1,33 +1,49 @@
 package com.mishasoft.androidsandbox;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+
+    private ListView listViewMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Log.d(getLocalClassName(), "message");
+        Log.d(getLocalClassName(), "started onCreate");
+
+        listViewMenu = (ListView) findViewById(R.id.listViewMenu);
+        listViewMenu.setOnItemClickListener(this);
+
+//        Log.d(getLocalClassName(), "finished onCreate");
     }
 
-    public void gotoSharedPreferences(View view) {
-        startActivity(new Intent(this, SharedPreferencesActivity.class));
-    }
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
 
-    public void gotoSavingFiles(View view) {
-        startActivity(new Intent(this, SavingFilesActivity.class));
-    }
+        Resources resources = getResources();
+        String selectedItem = resources.getStringArray(R.array.main_menu)[position];
 
-    public void gotoBluetooth(View view) {
-        startActivity(new Intent(this, BluetoothActivity.class));
-    }
-
-    public void gotoConnectingToWifi(View view) {
-        startActivity(new Intent(this, ConnectingToWifiActivity.class));
+        if (selectedItem.equals("Shared Preferences")) {
+            startActivity(new Intent(this, SharedPreferencesActivity.class));
+        } else if (selectedItem.equals("Saving Files")) {
+            startActivity(new Intent(this, SavingFilesActivity.class));
+        } else if (selectedItem.equals("Bluetooth")) {
+            startActivity(new Intent(this, BluetoothActivity.class));
+        }
     }
 }
